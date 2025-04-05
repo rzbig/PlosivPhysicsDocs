cf9970da9d99307bf0ce9d222cd3817084bcb083
rzbig <69272282+rzbig@users.noreply.github.com>
2025-04-05 19:43:57 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Plosiv Physics'
+copyright = '2025, Plosiv'
+author = 'Plosiv'
 
 release = '0.1'
 version = '0.1.0'
